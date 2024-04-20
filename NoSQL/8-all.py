5ed5333779f7b9f8fce0a9edc271199eac9b6e6f
pablosalina25 <casuarioventas@gmail.com>
2024-04-20 19:11:52 -0300

    8 fixed

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -5,6 +5,7 @@ List all documents in a MongoDB collection.
 
 
 def list_all(mongo_collection):
+    """Function that lists all documents in a collection"""
     cursor = mongo_collection.find()
     documents = list(cursor)
     return documents
