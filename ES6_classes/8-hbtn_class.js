c40153a4253a56b510afca7311af0a9d3e749df5
pablosalina25 <casuarioventas@gmail.com>
2024-05-04 09:15:54 -0700

    task 6 and eslint task 5

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,16 @@
+// Implement a class named HolbertonClass:
+
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  toString() {
+    return this._location;
+  }
+
+  valueOf() {
+    return this._size;
+  }
+}
