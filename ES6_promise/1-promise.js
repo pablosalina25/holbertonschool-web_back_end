1d57bfdf7384e9382884b50b8009bb4c1f3c4dff
pablosalina25 <casuarioventas@gmail.com>
2024-05-04 13:31:55 -0700

    fix task 1 and eslint

---
+++ ES6_promise/1-promise.js
@@ -0,0 +1,14 @@
+// Using the prototype below, return a promise. The parameter is a boolean
+
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      reject(Error('The fake API is not working currently'));
+    }
+  });
+}
