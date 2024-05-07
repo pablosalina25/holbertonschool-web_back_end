eb595f9cd365b13edde2c35d45b5e8171eb46293
pablosalina25 <casuarioventas@gmail.com>
2024-05-07 11:22:18 -0700

    fix

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,22 +1,19 @@
 // In this file, import uploadPhoto and createUser from utils.js
 
-import { createUser, uploadPhoto } from './utils';
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   const photoPromise = uploadPhoto();
+
   const userPromise = createUser();
 
   return Promise.all([photoPromise, userPromise])
-    .then((results) => {
-      const photoResult = results[0];
-      const userResult = results[1];
-
-      const { body: photoBody } = photoResult;
-      const { firstName, lastName } = userResult;
-
-      console.log(`Foto: ${photoBody}, Usuario: ${firstName} ${lastName}`);
+    .then(([photoResp, userResp]) => {
+      const { body } = photoResp;
+      const { firstName, lastName } = userResp;
+      console.log(`${body} ${firstName} ${lastName}`);
     })
     .catch(() => {
-      console.log(('Signup system offline'));
+      console.log('Signup system offline');
     });
 }
