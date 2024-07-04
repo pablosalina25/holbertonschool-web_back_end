e9e18703462b60dcf81613ee7580ac10fde7f008
pablosalina25 <casuarioventas@gmail.com>
2024-07-04 09:07:21 -0700

    5-typed_arrays.js

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,16 @@
+// Create a new ArrayBuffer with an Int8 value at a specific position
+function createInt8TypedArray(length, position, value) {
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
+
+  const buffer = new ArrayBuffer(length);
+
+  const view = new DataView(buffer);
+
+  view.setInt8(position, value);
+
+  return view;
+}
+
+export default createInt8TypedArray;
